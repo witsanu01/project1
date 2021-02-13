a925ddca661e78d956025761186338c6a2c5faca
peter120542 <61143245@cmru.ac.th>
2021-02-13 15:22:36 +0700

    database user

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body><a href="/">หน้าแรก</a> |<a href="/about">เกี่ยวกับ</a>|<a href="/contact">ติดต่อ</a>
-    <h1>witsanu Duangsang</h1>
-    <h1>well man hey</h1>
-    <h1>0810317893</h1>
-</body>
-</html>
+@extends('layout.master')
+
+@section('content')
+
+ <h1>0956981072</h1>
+ @endSection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
+@extends('layout.master')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">หน้าแรก</a> |<a href="/about">เกี่ยวกับ</a>|<a href="/contact">ติดต่อ</a>
- <h1>hi</h1>
-</body>
-</html>
+@section('content')
+
+ <h1>hello</h1>
+ @endSection
+
